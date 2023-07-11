df726a297fd031d4359259c3958453cddb449c60
¨Marianela <marufreigedo17@gmail.com>
2023-07-11 17:29:12 -0300

    Modifico mi proyecto

--- main.js
+++ main.js
@@ -1,32 +1,45 @@
 
-
 // Definimos el precio de las pizzas
-const precioMozzarella = 8.99;
-const precioNapolitana = 10.99;
+const precioMozzarella = 9;
+const precioNapolitana = 10;
 
 // Función para tomar el pedido y calcular el precio total
 function tomarPedido() {
   let total = 0;
 
-  // Mostramos las opciones de pizza y solicitamos al usuario que elija
+  // Mostramos las opciones de pizza
   console.log("Bienvenido a nuestra pizzería");
   console.log("Opciones de pizza:");
-  console.log("1. Mozzarella - $" + precioMozzarella);
-  console.log("2. Napolitana - $" + precioNapolitana);
+  
+  // Mostramos los productos disponibles con su precio
+  for (let i = 1; i <= 2; i++) {
+    let nombre = "";
+    let precio = 0;
+
+    if (i === 1) {
+      nombre = "Mozzarella";
+      precio = precioMozzarella;
+    } else if (i === 2) {
+      nombre = "Napolitana";
+      precio = precioNapolitana;
+    }
+
+    console.log(`${i}. ${nombre} - $${precio}`);
+  }
 
   // Solicitamos al usuario que ingrese su elección
   const opcion = prompt("Ingrese el número de la pizza que desea ordenar (0 para salir):");
 
   // Verificamos la elección del usuario y calculamos el precio total
-  if (opcion === "1") {
+  if (opcion === "0") {
+    console.log("Gracias por visitarnos. ¡Hasta luego!");
+    return; // Salimos de la función si el usuario elige salir
+  } else if (opcion === "1") {
     total = precioMozzarella;
     console.log("Ha elegido una pizza de Mozzarella");
   } else if (opcion === "2") {
     total = precioNapolitana;
     console.log("Ha elegido una pizza Napolitana");
-  } else if (opcion === "0") {
-    console.log("Gracias por visitarnos. ¡Hasta luego!");
-    return; // Salimos de la función si el usuario elige salir
   } else {
     console.log("Opción inválida. Por favor, elija una opción válida.");
     return; // Salimos de la función si la opción es inválida
