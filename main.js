30725fab4ff0d8f069859cdfd5c6a529d89fece2
¨Marianela <marufreigedo17@gmail.com>
2023-08-16 18:09:16 -0300

    modifico mi trabajo

--- main.js
+++ main.js
@@ -1,46 +1,52 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const pizzaSelect = document.getElementById('pizza-seleccion');
+  const foodSelect = document.getElementById('food-seleccion');
   const orderButton = document.getElementById('order-button');
   const resultArea = document.getElementById('result-area');
-  const pizzaList = document.getElementById('pizza-list'); 
+  const foodList = document.getElementById('food-list');
 
   // Cargar datos desde JSON y almacenarlos en localStorage
-  fetch('pizzas.json')
+  fetch('foods.json')
     .then(response => response.json())
     .then(data => {
-      localStorage.setItem('pizzas', JSON.stringify(data));
+      localStorage.setItem('foods', JSON.stringify(data));
 
-      data.forEach(pizza => {
+      // Agregar opciones de comida al menú desplegable
+      data.forEach(food => {
         const option = document.createElement('option');
-        option.value = pizza.nombre;
-        option.textContent = pizza.nombre;
-        pizzaSelect.appendChild(option);
-
-        const listItem = document.createElement('li'); // Agregado
-        listItem.textContent = `${pizza.nombre} - $${pizza.precio}`;
-        pizzaList.appendChild(listItem); // Agregado
+        option.value = food.nombre;
+        option.textContent = food.nombre;
+        foodSelect.appendChild(option);
       });
+
+      // Mostrar opciones de comida en la lista
+      updateFoodList(data);
     })
     .catch(error => console.error(error));
 
   // Mostrar datos de localStorage al cargar la página
-  const storedPizzas = JSON.parse(localStorage.getItem('pizzas'));
-  if (storedPizzas) {
-    storedPizzas.forEach(pizza => {
-      const listItem = document.createElement('li'); // Agregado
-      listItem.textContent = `${pizza.nombre} - $${pizza.precio}`;
-      pizzaList.appendChild(listItem); // Agregado
+  const storedFoods = JSON.parse(localStorage.getItem('foods'));
+  if (storedFoods) {
+    updateFoodList(storedFoods);
+  }
+
+  // Función para actualizar la lista de comida
+  function updateFoodList(foods) {
+    foodList.innerHTML = ''; // Limpiar la lista
+    foods.forEach(food => {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${food.nombre} - $${food.precio}`;
+      foodList.appendChild(listItem);
     });
   }
 
   // Asociar evento al botón de pedido
   orderButton.addEventListener('click', () => {
-    const selectedPizzaNombre = pizzaSelect.value;
-    const pizzas = JSON.parse(localStorage.getItem('pizzas'));
-    const selectedPizza = pizzas.find(pizza => pizza.nombre === selectedPizzaNombre);
+    const selectedFoodNombre = foodSelect.value;
+    const foods = JSON.parse(localStorage.getItem('foods'));
+    const selectedFood = foods.find(food => food.nombre === selectedFoodNombre);
 
-    if (selectedPizza) {
-      resultArea.textContent = `Ha elegido una pizza de ${selectedPizza.nombre}. El precio total es: $${selectedPizza.precio}`;
+    if (selectedFood) {
+      resultArea.textContent = `Ha elegido ${selectedFood.nombre}. El precio total es: $${selectedFood.precio}`;
     }
   });
 });
