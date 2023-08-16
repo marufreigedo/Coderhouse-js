0398f91a042c1ed4f85c64ff6450a2b8d94f6364
¨Marianela <marufreigedo17@gmail.com>
2023-08-16 17:52:55 -0300

    modifico mi pagina

--- main.js
+++ main.js
@@ -1,33 +1,46 @@
 document.addEventListener("DOMContentLoaded", function() {
+  const pizzaSelect = document.getElementById('pizza-seleccion');
+  const orderButton = document.getElementById('order-button');
+  const resultArea = document.getElementById('result-area');
+  const pizzaList = document.getElementById('pizza-list'); 
+
   // Cargar datos desde JSON y almacenarlos en localStorage
   fetch('pizzas.json')
     .then(response => response.json())
     .then(data => {
       localStorage.setItem('pizzas', JSON.stringify(data));
-      const pizzaSelect = document.getElementById('pizza-seleccion');
+
       data.forEach(pizza => {
         const option = document.createElement('option');
         option.value = pizza.nombre;
         option.textContent = pizza.nombre;
         pizzaSelect.appendChild(option);
+
+        const listItem = document.createElement('li'); // Agregado
+        listItem.textContent = `${pizza.nombre} - $${pizza.precio}`;
+        pizzaList.appendChild(listItem); // Agregado
       });
     })
     .catch(error => console.error(error));
 
+  // Mostrar datos de localStorage al cargar la página
+  const storedPizzas = JSON.parse(localStorage.getItem('pizzas'));
+  if (storedPizzas) {
+    storedPizzas.forEach(pizza => {
+      const listItem = document.createElement('li'); // Agregado
+      listItem.textContent = `${pizza.nombre} - $${pizza.precio}`;
+      pizzaList.appendChild(listItem); // Agregado
+    });
+  }
+
   // Asociar evento al botón de pedido
-  const orderButton = document.getElementById('order-button');
   orderButton.addEventListener('click', () => {
-    const selectedPizzaNombre = document.getElementById('pizza-seleccion').value;
+    const selectedPizzaNombre = pizzaSelect.value;
     const pizzas = JSON.parse(localStorage.getItem('pizzas'));
     const selectedPizza = pizzas.find(pizza => pizza.nombre === selectedPizzaNombre);
 
     if (selectedPizza) {
-      const resultArea = document.getElementById('result-area');
       resultArea.textContent = `Ha elegido una pizza de ${selectedPizza.nombre}. El precio total es: $${selectedPizza.precio}`;
     }
   });
-  
 });
-
-
-
